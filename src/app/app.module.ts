534d79c8f5fc11f243227bc0ce3f4581b168f431
Jason DiBabbo <1293421+JasonDiBabbo@users.noreply.github.com>
2021-01-15 03:45:42 -0600

    Added skeleton for widget control component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WidgetAreaComponent } from './widget-area/widget-area.component';
+import { WidgetControlComponent } from './widget-control/widget-control.component';
 
 @NgModule({
-  declarations: [AppComponent, WidgetAreaComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, WidgetControlComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
